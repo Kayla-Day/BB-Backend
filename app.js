fb6f31913805ae18063411a150b1668521c40b62
Kayla-Day <kayladayguimont@gmail.com>
2023-07-28 20:15:58 -0500

    minor

--- app.js
+++ app.js
@@ -197,14 +197,13 @@ app.get("/free-endpoint", (request, response) => {
 // });
 
 app.get("/auth-endpoint", auth, (request, response) => {
-  const user = request.user;
   // const email = user.email;
   // const balance = user.balance;
 
   response.send({
     message: "",
-    email: user.email,
-    balance: user.balance,
+    email: User.email,
+    balance: User.balance,
   });
 
   // response.send({
