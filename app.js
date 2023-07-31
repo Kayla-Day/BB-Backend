5a32cc0261031c57deda3d7098eaffb5918d6915
Kayla-Day <kayladayguimont@gmail.com>
2023-07-31 17:28:23 -0500

    minor

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ app.get("/free-endpoint", (request, response) => {
 // protected endpoint
 app.get("/auth-endpoint", auth, async (request, response) => {
   const user = await User.findOne({ email: request.user.userEmail });
-  response.json({ message: "test auth message", user: user });
+  response.json({ message: "Complete transactions below", user: user });
 });
 
 module.exports = app;
