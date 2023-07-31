685accbf498bd2a459e50fe367bc6bbfb223fafb
Kayla-Day <kayladayguimont@gmail.com>
2023-07-31 16:45:29 -0500

    reverted

--- app.js
+++ app.js
@@ -202,15 +202,8 @@ app.get("/free-endpoint", (request, response) => {
 
 // protected endpoint
 app.get("/auth-endpoint", auth, async (request, response) => {
-  const user = await User.findOne({
-    email: request.user.userEmail,
-    balance: request.user.userBalance,
-  });
-  response.json({
-    message: " ",
-    user: user,
-    balance: user.balance,
-  });
+  const user = await User.findOne({ email: request.user.userEmail });
+  response.json({ message: "test auth message", user: user });
 });
 
 module.exports = app;
